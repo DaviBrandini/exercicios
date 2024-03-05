27fb62b63f934b21dd86de9d5eeed1e358587cd0
DAVI <davishillebrand8@gmail.com>
2024-03-05 11:48:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//comando para exibir mensagem
+console.log("O INTER VAI SAIR CAMPEÃO E O GRÊMIO VAI CHUPAR DEDO")
+
+// exercicio de calculos
+
+const num1 = 20;
+const num2 = 654;
+
+// soma 
+let soma = num1 + num2;
+
+console.log("O valor da soma é " + soma); 
+
+let subtração = num1 - num2;
+console. log("O valor da subtração é " + subtração + " COLORADO MAIOR DO SUL ");
+ 
+let multiplicação = num1 * num2;
+console. log("O valor da multiplicação é " + multiplicação + " COLORADO MAIOR DO SUL");
+ 
+let divisão = num1 / num2;
+console. log("O valor da divisão é " + divisão + " COLORADO MAIOR DO SUL");
